b1bd58d9cd72adfc3e31221e105e5cf7fcb81e74
Support <Support@WEMA-TRAINING-S>
2022-08-05 12:16:45 +0100

    Add project files.

---
+++ Abstract/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstractt
+{
+    // this is an abstract class
+    internal abstract class Company
+    {
+        // boss and role does not return any value
+        public abstract void boss();
+        public void role()
+        {
+            Console.WriteLine("RhoyalSam is: Head of the company");
+        }
+    }
+    //This class was inherited from the class Family
+    class Employee : Company
+            {
+        public override void boss()
+        {
+            Console.WriteLine("Employee is: first worker");
+        }
+    }
+
+}
---
+++ Abstract/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Abstractt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee myemployee = new Employee();
+            myemployee.boss();
+            myemployee.role();
+        }
+    }
+}
